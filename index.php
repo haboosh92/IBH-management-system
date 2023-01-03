29cceb9514135712d9a7ce0d37e7a2114ff08caf
Hiba <eng.lilil92@gmail.com>
2023-01-03 11:37:53 +0300

    db update

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!-- <?php
-// require_once ("include/initialize.php");
-
-// $content='admin/login.php';	
-    
-// require_once("theme/templates.php");
-?>
- -->
 
  <?php
 require_once("include/initialize.php");
